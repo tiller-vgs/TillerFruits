bdd61b263bace84b4e4a192ff115ea75aea86acb
plAsk <askryenvolden@hotmail.com>
2026-04-29 12:37:05 +0200

    style:change background color
    Closes:#17

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-  <div className="flex flex-col items-center mt-auto justify-center p-5 min-w-full bg-red-700">
-    <h1 className="text-xl font-bold">Tiller VGS</h1>
+  <div className="flex flex-col items-center mt-auto justify-center p-5 min-w-full bg-transparent">
+    <h1 className="text-xl font-bold text-[#1F1300]">Tiller VGS</h1>
     <h2 className="text-lg">2026</h2>
 
   </div>
