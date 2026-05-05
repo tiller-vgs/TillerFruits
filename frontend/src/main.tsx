1481f63ffcdd6fc59460469235740c4624fd1079
Henrik <henalyn23@hotmail.com>
2026-05-05 12:37:03 +0200

    feat: LoginCard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import HomePage from "./HomePage.tsx";
 import Navbar from "./components/Navbar.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./landingPage";
+import LoginCard from "./components/LoginCard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginCard />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
