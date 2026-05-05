a3c26de9d2f450629b8155520aba0101ff72eeb5
plAsk <askryenvolden@hotmail.com>
2026-05-05 14:16:12 +0200

    feat: fixed button style

--- frontend/src/components/UploadButton.tsx
+++ frontend/src/components/UploadButton.tsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 function UploadButton({ fileInputRef, handleFileInput }) {
   return (
@@ -9,9 +10,24 @@ function UploadButton({ fileInputRef, handleFileInput }) {
         className="hidden"
         onChange={handleFileInput}
         multiple
+
       />
       <Button
-        sx={{ fontSize: "1.25rem", mb: "0.5rem"}}
+      startIcon={<FileUploadIcon sx={{ fontSize: "4rem", color: "#ffffff", }} />}
+        sx={{mt: "1rem",
+          fontSize: "1.1rem",
+          mb: "0.5rem",
+          padding: "0.6rem 1.5rem",
+          borderRadius: "10px",
+          textTransform: "none", 
+          backgroundColor: "#1F1300",
+          color: "#F6F7F8",
+          transition: "0.2s ease",
+          ":hover": {backgroundColor: "#6D5A72",
+          transform: "translateY(-2px)"
+    }
+  }}
+
         variant="contained"
         onClick={() => fileInputRef.current.click()}
       >
