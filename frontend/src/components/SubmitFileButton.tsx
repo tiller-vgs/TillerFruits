f1ebfd91e5df8ee533abba85226d722cf28517ee
Noah <noahdiakowski@gmail.com>
2026-05-08 18:35:22 +0200

    refactor: moved utils folder into src

--- frontend/src/components/SubmitFileButton.tsx
+++ frontend/src/components/SubmitFileButton.tsx
@@ -1,6 +1,6 @@
 import Button from "@mui/material/Button";
 import type { UploadFileType, UploadStatus } from "../../types/types";
-import handleSendingOfFile from "../../utils/handleSendingOfFile";
+import handleSendingOfFile from "../utils/handleSendingOfFile";
 
 function SubmitFileButton({
   addedFile,
@@ -14,25 +14,25 @@ function SubmitFileButton({
   return (
     <Button
       variant="outlined"
-      sx={{mt: "1rem",
-          fontSize: "1.1rem",
-          mb: "0.5rem",
-          padding: "0.6rem 1.5rem",
-          borderRadius: "10px",
-          textTransform: "none", 
-          backgroundColor: "#1F1300",
-          color: "#F6F7F8",
-          transition: "0.2s ease",
-          ":hover": {backgroundColor: "#6D5A72",
-          transform: "translateY(-2px)"
-    }
-  }}
-
-        onClick={() => handleSendingOfFile({ addedFile, setUploadStatus, setErrorMessage })}
+      sx={{
+        mt: "1rem",
+        fontSize: "1.1rem",
+        mb: "0.5rem",
+        padding: "0.6rem 1.5rem",
+        borderRadius: "10px",
+        textTransform: "none",
+        backgroundColor: "#1F1300",
+        color: "#F6F7F8",
+        transition: "0.2s ease",
+        ":hover": { backgroundColor: "#6D5A72", transform: "translateY(-2px)" },
+      }}
+      onClick={() =>
+        handleSendingOfFile({ addedFile, setUploadStatus, setErrorMessage })
+      }
     >
       Submit
     </Button>
   );
 }
 
-export default SubmitFileButton
+export default SubmitFileButton;
