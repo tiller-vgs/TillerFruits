60996606c26a7a7c0aecb043f9d8ae21ae356906
Noah <noahdiakowski@gmail.com>
2026-05-14 13:09:54 +0200

    feat: add error handling and alerts
    refs:#54

--- frontend/src/pages/AssignmentDistribute.tsx
+++ frontend/src/pages/AssignmentDistribute.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import FormHelperText from "@mui/material/FormHelperText";
 import handleDistribution from "../utils/handleDistribution";
 import type { FileTypeFromDB } from "../../types/types";
+import Alert from "@mui/material/Alert";
 
 export default function AssignmentDistribute() {
   const { id } = useParams();
@@ -13,6 +14,7 @@ export default function AssignmentDistribute() {
   const [file, setFile] = useState<FileTypeFromDB | null>(null);
   const [isSent, setIsSent] = useState<boolean>(false);
   const [studentAmount, setStudentAmount] = useState<number>(0);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
     async function fetchFile() {
@@ -26,33 +28,45 @@ export default function AssignmentDistribute() {
         if (!response.ok) {
           throw new Error(data.message);
         }
-        console.log("FetchFile in assignmentdis: ", data.data);
         setFile(data.data);
-      } catch (err) {
+        setErrorMessage("");
+      } catch (err: any) {
         console.error(err);
+        setFile(null);
+        setErrorMessage(err.message || "No file found");
       }
     }
 
     fetchFile();
   }, [fileId]);
 
+  const isPdf = file?.extension === "pdf";
+
   return (
     <main className="flex flex-row min-h-[calc(100dvh-65px)] w-full p-5 justify-between">
-      <div className="flex flex-col w-2/3">
+      <div className="flex flex-col w-2/3 gap-3">
         <h1>
           [{file?.extension.toUpperCase()}] - sendt av:{" "}
           <span className="font-bold">{file?.displayName}</span>
         </h1>
 
-        {file ? (
+        {errorMessage && (
+          <Alert severity="error" sx={{ fontSize: "1.1rem" }}>
+            {errorMessage}
+          </Alert>
+        )}
+
+        {file && isPdf ? (
           <div className="max-h-[80dvh] overflow-y-auto flex flex-col gap-0 p-0 border-black border-2 rounded-2xl">
             <FilePreview
               fileURL={`http://localhost:5000/api/v1/files/${file.id}/content`}
             />
           </div>
-        ) : (
-          <div>No file found. Contact support.</div>
-        )}
+        ) : file ? (
+          <Alert severity="error" sx={{ fontSize: "1.1rem", width:"60%"}}>
+            Forhåndsvisning er tilgjengelig kun for PDF-filer.
+          </Alert>
+        ) : null}
       </div>
 
       <div className="w-1/2">
